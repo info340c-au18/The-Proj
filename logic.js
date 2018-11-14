58b305f54d16b8c4f31b265147009d70d92191b0
sch <robert7kim7@gmail.com>
2018-11-13 21:53:03 -0800

    extra changes

--- logic.js
+++ logic.js
@@ -1,14 +1,21 @@
-
-
-
 'use strict';
 
-function fetchTrackList() {
-    // console.log("it works: " + searchTerm);
+// On load of Window, these functions will be run:
+window.onload = function () {
+    $("#getSongs").click(function () {
+        $("a").remove(".songEl");
+        fetchTrackList();
+    });
 
+    $("#getArtists").click(function () {
+        $("ol").remove(".artist-cont")
+        fetchArtists();
+    });
+};
+
+// Fetches API Content of Tracks:
+function fetchTrackList() {
     let apiKey = "3b7073a687cc95da319e6dff74046fac";
-    // const URL_TEMPLATE = "http://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=" + akiKey + "&format=json";
-    // const URL_TEMPLATE = "http://api.napster.com/v2.2/albums/new?apikey=YTkxZTRhNzAtODdlNy00ZjMzLTg0MWItOTc0NmZmNjU4Yzk4&limit=5?genre=pop";
     const URL_TEMPLATE = "http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=3b7073a687cc95da319e6dff74046fac&format=json";
 
     return fetch(URL_TEMPLATE)
@@ -24,6 +31,8 @@ function fetchTrackList() {
         })
 }
 
+// Retrieves the information on the current top songs (name, artist name, url to play song, image url), 
+// then injects them into the HTML. 
 function getTracks(data) {
     let trackArr = data['tracks']['track'];
     // console.log(trackArr);
@@ -32,7 +41,6 @@ function getTracks(data) {
     var temp = "";
     var count = 0;
     var uniqueArtists = [];
-
 
     for (var i = 0; i < 50; i++) {
         let songName = trackArr[i].name;
@@ -53,12 +61,8 @@ function getTracks(data) {
     }
 }
 
-
+// Helper: Takes in a single song object and injects into the HTML. 
 function injectTopSongs(singleSong) {
-    console.log(singleSong.artistName);
-
-    console.log(singleSong.urlToPlaySong);
-
     var songEl = $('<div class="songEl"> </div>');
     var songEl = $('<a class="songEl" target="_blank"  href=' + singleSong.urlToPlaySong + '></a>');
     songEl.css("position", "relative");
@@ -80,36 +84,24 @@ function injectTopSongs(singleSong) {
         });
     });
 
-
     var imgEl = $('<img class="imageEl" src =' + singleSong.albumLgImg + '>');
     imgEl.css({ 'display': 'inline-block', "width": "100%", "margin-right": "5px", "background-repeat": "no-repeat", "background-size": "cover" });
-    // imgEl.addClass('img-change');
 
     var textEl = $('<h2 class="song-name"></h2>');
     textEl.text(singleSong.songName);
     textEl.css({ 'background-color': 'white', 'opacity': '.92', 'padding': '5px', 'position': 'absolute', 'top': '50%', 'left': '50%', 'transform': 'translate(-50%, -50%)' });
-    // textEl.css("font-size", "8px");
 
     songEl.append(textEl);
     songEl.append(imgEl);
-    // songEl.append(buttonEl);
     songEl.css({ 'display': 'inline-block', "height": "100%", "margin-right": "5px", "background-repeat": "no-repeat", "background-size": "cover" });
     songEl.addClass("respoSongEl");
 
-    // var clickDirect = $('<p>');
-    // clickDirect.text("{ Click on Album Covers to open the Songs}");
-
-    // $(".topSongsCont").append(clickDirect);
-
-
     $(".topSongsCont").append(songEl);
-
 }
 
+// Fetches API Content for Artists:
 function fetchArtists() {
     let apiKey = "3b7073a687cc95da319e6dff74046fac";
-    // const URL_TEMPLATE = "http://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=" + akiKey + "&format=json";
-    // const URL_TEMPLATE = "http://api.napster.com/v2.2/albums/new?apikey=YTkxZTRhNzAtODdlNy00ZjMzLTg0MWItOTc0NmZmNjU4Yzk4&limit=5?genre=pop";
     const URL_TEMPLATE = "http://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=3b7073a687cc95da319e6dff74046fac&format=json";
 
     return fetch(URL_TEMPLATE)
@@ -122,63 +114,37 @@ function fetchArtists() {
         })
 }
 
+// Helper: Formats the number into str, and adds commas:
 function numberWithCommas(number) {
     var parts = number.toString().split(".");
     parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     return parts.join(".");
 }
 
+// Retrieves information about Artist (name, listener count, and image url)
 function getArtists(data) {
     let artistArr = data['artists']['artist'];
 
-    // console.log(artistLgImg);
-    // var playCount = artistArr.playcount;
-
     var olElem = $('<ol> </ol>');
     for (var i = 0; i < 6; i++) {
-        // Create Image Tag: 
         let artistMdImgLink = artistArr[i]['image'][1]['#text'];
-        // console.log(artistMdImgLink);
         var imgElStr = "<img class='artist-pic' src='" + artistMdImgLink + "'>";
-        // console.log(imgEl);
 
-        // artist name and number of listeners:
         let artistName = artistArr[i].name;
         var numListeners = artistArr[i].listeners;
         numListeners = numberWithCommas(numListeners);
-        // console.log(artistName);
-        // console.log(numListeners);
         var textStr = "<strong>Name of Artist:</strong> " + artistName + " <br> <strong># of Listeners:</strong> " + numListeners;
         console.log(textStr);
 
         var pContainerEl = $('<p class="artist-bio">' + imgElStr + textStr + '</p>');
-
         var liEl = $('<li class="artist-li">' + '</li>');
         liEl.append(pContainerEl);
-        // var liEl = $('<li>' + imgElStr + textStr + '</li>');
 
-
-
-
-        // console.log(combStr);
         olElem.append(liEl);
     }
 
     olElem.addClass('artist-cont');
-
     $('.topArtistsCont').append(olElem);
 }
 
 
-
-window.onload = function () {
-    $("#getSongs").click(function () {
-        fetchTrackList();
-    });
-
-    $("#getArtists").click(function () {
-        fetchArtists();
-    });
-
-
-};
