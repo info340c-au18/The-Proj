019a2190438e40fa728d0025b231020e38ed9221
sch <robert7kim7@gmail.com>
2018-11-14 18:59:26 -0800

    bugs

--- logic.js
+++ logic.js
@@ -10,16 +10,11 @@ window.onload = function () {
         $("ol").remove(".artist-cont");
         fetchArtists();
     });
-
-
 };
 
+// Calls API and retrieves information on the Top Songs right now: 
 function fetchTrackList() {
-    // console.log("it works: " + searchTerm);
-
     let apiKey = "3b7073a687cc95da319e6dff74046fac";
-    // const URL_TEMPLATE = "http://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=" + akiKey + "&format=json";
-    // const URL_TEMPLATE = "http://api.napster.com/v2.2/albums/new?apikey=YTkxZTRhNzAtODdlNy00ZjMzLTg0MWItOTc0NmZmNjU4Yzk4&limit=5?genre=pop";
     const URL_TEMPLATE = "https://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=3b7073a687cc95da319e6dff74046fac&format=json";
 
     return fetch(URL_TEMPLATE)
@@ -35,16 +30,16 @@ function fetchTrackList() {
         })
 }
 
+// Retrieves specific information (song name, artost name, url to song, album image) on each song: 
 function getTracks(data) {
     let trackArr = data['tracks']['track'];
-    // console.log(trackArr);
 
     // Skip to a unique artist:
     var temp = "";
     var count = 0;
     var uniqueArtists = [];
 
-
+    // Gets the top 50 popular songs (extra may be used for adding on later features):
     for (var i = 0; i < 50; i++) {
         let songName = trackArr[i].name;
         let artistName = trackArr[i]['artist'].name;
@@ -64,12 +59,8 @@ function getTracks(data) {
     }
 }
 
-
+// Injects all the gathered information on each song, creates html elements and styles, and injects into the DOM.
 function injectTopSongs(singleSong) {
-    console.log(singleSong.artistName);
-
-    console.log(singleSong.urlToPlaySong);
-
     var songEl = $('<div class="songEl"> </div>');
     var songEl = $('<a class="songEl" target="_blank"  href=' + singleSong.urlToPlaySong + '></a>');
     songEl.css("position", "relative");
@@ -91,36 +82,23 @@ function injectTopSongs(singleSong) {
         });
     });
 
-
     var imgEl = $('<img class="imageEl" src =' + singleSong.albumLgImg + '>');
     imgEl.css({ 'display': 'inline-block', "width": "100%", "margin-right": "5px", "background-repeat": "no-repeat", "background-size": "cover" });
-    // imgEl.addClass('img-change');
-
     var textEl = $('<h2 class="song-name"></h2>');
     textEl.text(singleSong.songName);
     textEl.css({ 'background-color': 'white', 'opacity': '.92', 'padding': '5px', 'position': 'absolute', 'top': '50%', 'left': '50%', 'transform': 'translate(-50%, -50%)' });
-    // textEl.css("font-size", "8px");
 
     songEl.append(textEl);
     songEl.append(imgEl);
-    // songEl.append(buttonEl);
     songEl.css({ 'display': 'inline-block', "height": "100%", "margin-right": "5px", "background-repeat": "no-repeat", "background-size": "cover" });
     songEl.addClass("respoSongEl");
 
-    // var clickDirect = $('<p>');
-    // clickDirect.text("{ Click on Album Covers to open the Songs}");
-
-    // $(".topSongsCont").append(clickDirect);
-
-
     $(".topSongsCont").append(songEl);
-
 }
 
+// Calls API and retrieves information on the Top Artists right now: 
 function fetchArtists() {
     let apiKey = "3b7073a687cc95da319e6dff74046fac";
-    // const URL_TEMPLATE = "http://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=" + akiKey + "&format=json";
-    // const URL_TEMPLATE = "http://api.napster.com/v2.2/albums/new?apikey=YTkxZTRhNzAtODdlNy00ZjMzLTg0MWItOTc0NmZmNjU4Yzk4&limit=5?genre=pop";
     const URL_TEMPLATE = "https://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=3b7073a687cc95da319e6dff74046fac&format=json";
 
     return fetch(URL_TEMPLATE)
@@ -133,51 +111,79 @@ function fetchArtists() {
         })
 }
 
+// Helper: Takes in a number, converts to string, and adds commas for big numbers: 
 function numberWithCommas(number) {
     var parts = number.toString().split(".");
     parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     return parts.join(".");
 }
 
+// Retrieves specific information (image url, artist name, number of listeners) on each Artist: 
 function getArtists(data) {
     let artistArr = data['artists']['artist'];
-
-    // console.log(artistLgImg);
-    // var playCount = artistArr.playcount;
-
     var olElem = $('<ol> </ol>');
+
+    var xCoords = [];
+    var yCoords = [];
+
+    // Gets only 6 Artists: 
     for (var i = 0; i < 6; i++) {
         // Create Image Tag: 
         let artistMdImgLink = artistArr[i]['image'][1]['#text'];
-        // console.log(artistMdImgLink);
         var imgElStr = "<img class='artist-pic' src='" + artistMdImgLink + "'>";
-        // console.log(imgEl);
 
         // artist name and number of listeners:
         let artistName = artistArr[i].name;
         var numListeners = artistArr[i].listeners;
         numListeners = numberWithCommas(numListeners);
-        // console.log(artistName);
-        // console.log(numListeners);
         var textStr = "<strong>Name of Artist:</strong> " + artistName + " <br> <strong># of Listeners:</strong> " + numListeners;
-        console.log(textStr);
 
         var pContainerEl = $('<p class="artist-bio">' + imgElStr + textStr + '</p>');
-
         var liEl = $('<li class="artist-li">' + '</li>');
         liEl.append(pContainerEl);
-        // var liEl = $('<li>' + imgElStr + textStr + '</li>');
-
-
-
-
-        // console.log(combStr);
         olElem.append(liEl);
+
+        xCoords.push(artistName);
+        yCoords.push(numListeners);
     }
 
-    olElem.addClass('artist-cont');
 
+    // Prepares data / settings for plot:
+    var data = [
+        {
+            x: xCoords,
+            y: yCoords,
+            type: 'bar',
+            marker: {
+                color: '#ff3333',
+            }
+        }
+    ];
+
+    // Creates a layout to plot:
+    var layout = {
+        title: 'Top Artists Right Now:',
+        font: {
+            family: 'Raleway, sans-serif'
+        },
+        showlegend: false,
+        xaxis: {
+            tickangle: -45
+        },
+        yaxis: {
+            zeroline: false,
+            gridwidth: 2
+        },
+        bargap: 0.05
+    };
+
+    // Adds plot with the settings (layout, data) to DOM:
+    Plotly.newPlot('plotDiv', data, layout);
+
+    // Adds to the DOM: 
+    olElem.addClass('artist-cont');
     $('.topArtistsCont').append(olElem);
+
 }
 
 
